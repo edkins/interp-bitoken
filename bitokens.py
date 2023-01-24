6191d97dafa4e4f1465e7131f792062422b66d17
Giles Edkins <edkins@gmail.com>
2023-01-24 10:59:00 -0500

    Pick more convenient sentences

--- bitokens.py
+++ bitokens.py
@@ -13,33 +13,45 @@ model = HookedTransformer.from_pretrained('gpt2-small', device=device)
 bitokens = ['shear', 'binomial', 'tangent', 'detergent', 'leaped', 'parlor', 'textiles', 'fastened', 'lagoon', 'inventories', 'painters']
 
 for bitoken in bitokens:
-    for s in get_sentences_containing(bitoken)[:1]:
-        print(s)
+    best_score = -10000000000
+    best_s = None
+    for s in get_sentences_containing(bitoken):
         tokens, highlighted = tokenize_and_highlight(model.tokenizer, s, bitoken)
-        strtokens = model.to_str_tokens(s)
-        ttokens = torch.tensor([tokens], device=device)
-        logits, cache = model.run_with_cache(ttokens)
-        fig, ax = plt.subplots(model.cfg.n_layers, model.cfg.n_heads, squeeze=False)
-        for layer in range(model.cfg.n_layers):
-            attn = cache['pattern', layer, 'attn'][0,:,:,:].cpu()
-            for head in range(model.cfg.n_heads):
-                colors = torch.zeros((len(tokens), len(tokens), 3))
-                for i in range(len(tokens)):
-                    for j in range(len(tokens)):
-                        value = math.sqrt(attn[head,i,j])
-                        if highlighted[j] and j<len(tokens)-1 and highlighted[j+1]:
-                            if i == j or i == j+1:
-                                colors[i,j,0] = value
-                                colors[i,j,1] = value
-                                colors[i,j,2] = value
-                            else:
-                                colors[i,j,0] = value
-                                colors[i,j,1] = min(value, math.sqrt(attn[head,i,j+1]))
-                        elif highlighted[j]:
+        # Want at least 4 tokens after the highlighted ones. Otherwise the shorter the better.
+        if True in highlighted and highlighted.index(True) + 4 < len(highlighted):
+            score = -len(tokens)
+            if score > best_score:
+                best_score = score
+                best_s = s
+
+    if best_s is None:
+        continue
+    s = best_s
+    print(s)
+    tokens, highlighted = tokenize_and_highlight(model.tokenizer, s, bitoken)
+    ttokens = torch.tensor([tokens], device=device)
+    logits, cache = model.run_with_cache(ttokens)
+    fig, ax = plt.subplots(model.cfg.n_layers, model.cfg.n_heads, squeeze=False)
+    for layer in range(model.cfg.n_layers):
+        attn = cache['pattern', layer, 'attn'][0,:,:,:].cpu()
+        for head in range(model.cfg.n_heads):
+            colors = torch.zeros((len(tokens), len(tokens), 3))
+            for i in range(len(tokens)):
+                for j in range(len(tokens)):
+                    value = math.sqrt(attn[head,i,j])
+                    if highlighted[j] and j<len(tokens)-1 and highlighted[j+1]:
+                        if i == j or i == j+1:
+                            colors[i,j,0] = value
                             colors[i,j,1] = value
-                        else:
                             colors[i,j,2] = value
-                ax[layer, head].set_axis_off()
-                ax[layer, head].imshow(colors)
-        fig.suptitle(s)
-        plt.show()
+                        else:
+                            colors[i,j,0] = value
+                            colors[i,j,1] = min(value, math.sqrt(attn[head,i,j+1]))
+                    elif highlighted[j]:
+                        colors[i,j,1] = value
+                    else:
+                        colors[i,j,2] = value
+            ax[layer, head].set_axis_off()
+            ax[layer, head].imshow(colors)
+    fig.suptitle(s)
+    plt.show()
